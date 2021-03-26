ea0031a072685680733ecb28fc1badd66097f26d
bloodmurderer1 <61007754+bloodmurderer1@users.noreply.github.com>
2021-03-26 17:54:38 +0800

    Sort of Working Menu

--- 3D Arcade/Assets/Scripts/MainMenu.cs
+++ 3D Arcade/Assets/Scripts/MainMenu.cs
@@ -8,19 +8,24 @@ public class MainMenu : MonoBehaviour
 {
     public void Update()
     {
-        if (Input.GetMouseButton(0) || SAE.ArcadeMachine.PlayerPressingButtonStatic(SAE.ArcadeMachine.PlayerColorId.YELLOW_PLAYER, 3) == true)
+        if (Input.GetMouseButton(0) || SAE.ArcadeMachine.PlayerPressingButtonStatic(SAE.ArcadeMachine.PlayerColorId.YELLOW_PLAYER, 4) == true)
         {
             PlayGame();
         }
 
-        if (Input.GetMouseButton(0) || SAE.ArcadeMachine.PlayerPressingButtonStatic(SAE.ArcadeMachine.PlayerColorId.YELLOW_PLAYER, 4) == true)
+        if (Input.GetMouseButton(0) || SAE.ArcadeMachine.PlayerPressingButtonStatic(SAE.ArcadeMachine.PlayerColorId.YELLOW_PLAYER, 3) == true)
         {
             PlayCoopGame();
         }
 
-        if (Input.GetMouseButton(0) || SAE.ArcadeMachine.PlayerPressingButtonStatic(SAE.ArcadeMachine.PlayerColorId.YELLOW_PLAYER, 1) == true)
+        if (Input.GetMouseButton(0) || SAE.ArcadeMachine.PlayerPressingButtonStatic(SAE.ArcadeMachine.PlayerColorId.YELLOW_PLAYER, 7) == true)
         {
             QuitGame();
+        }
+
+        if(Input.GetMouseButton(0) || SAE.ArcadeMachine.PlayerPressingButtonStatic(SAE.ArcadeMachine.PlayerColorId.YELLOW_PLAYER, 6) == true)
+        {
+            SceneManager.LoadScene(0);
         }
 
     }
@@ -30,7 +35,8 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         Debug.Log("Play Game");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(1);
     }
     
     public void QuitGame()
@@ -42,7 +48,8 @@ public class MainMenu : MonoBehaviour
     public void PlayCoopGame()
     {
         Debug.Log("Play Co-op Game");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        SceneManager.LoadScene(2);
     }
 
 }
